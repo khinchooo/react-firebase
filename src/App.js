d59e0fbef6b75c8ddd68ceda767d8fb8107349cb
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-02-14 18:22:45 +0900

    react-router-dom追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
       <SignUp />
     </div>
   );
