daec4991fe06fd4709203fc5ef9e24ca54eb995e
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-02-14 18:33:07 +0900

    Home とLoginページを追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home />}></Route>
           <Route path="/signup" element={<SignUp />} />
+          <Route path='/login' element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
       <SignUp />
