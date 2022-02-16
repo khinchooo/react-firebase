d337405d1954788e07a43ca4a6b389db8a44b75a
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-02-16 18:18:24 +0900

    Firestoreアクセス

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import { signOut } from "firebase/auth";
 import { useNavigate, Navigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { useAuthContext } from "../context/AuthContext";
+import { useState, useEffect } from "react";
+import { collection, getDocs } from 'firebase/firestore';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -10,14 +12,32 @@ const Home = () => {
     navigate('/login');
   };
   const { user } = useAuthContext();
-  console.log("home: ", user);
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const usersCollectionRef = collection(db, 'users');
+    getDocs(usersCollectionRef).then((querySnapshot) => {
+      // querySnapshot.docs.forEach((doc) =>
+      //   console.log("###HOME###", doc.data())
+      // );
+      setUsers(querySnapshot.docs.map((doc) => doc.data()))
+    });
+  }, []);
+
   if (!user) {
     return <Navigate to="/login" />;
   } else {
     return (
       <div>
         <h1>ホームページ</h1>
+        <div>
         <button onClick={handleLogout}>ログアウト</button>
+        </div>
+        <div>
+          {users.map((user) => (
+            <div key={user.id}>{user.name}</div>
+          ))}
+        </div>
       </div>
     );
   };
